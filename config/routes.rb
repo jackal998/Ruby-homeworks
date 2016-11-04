9baa19cf174ba676e8081a9254baacd45f8026f2
jackal998 <jackal998@gmail.com>
2016-11-04 13:41:27 +0800

    modified__form_index_new_routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # 路由收到 request 後，會執行 "events" controller 中的 "index" action
   # 且照慣例會使用同名的 "index".html.erb 回傳 response 給使用者
   # match ':controller(/:action(/:id(.:format)))', :via => :all
+  
 end
